03c6f9550597970c796b59a5b64fe333b16c2126
Paul <pfultz2@yahoo.com>
2014-07-28 21:32:06 -0400

    Update documentation for partial

--- fit/partial.h
+++ fit/partial.h
@@ -8,38 +8,38 @@
 #ifndef FIT_GUARD_FUNCTION_PARTIAL_H
 #define FIT_GUARD_FUNCTION_PARTIAL_H
 
-// partial
-// ========
-// 
-// Description
-// -----------
-// 
-// The `partial` function adaptor allows partial application of the function.
-// If the function can not be called with all the parameters, it will return a
-// function. It will continually do this until the function can be called. By
-// default, the `partial` captures all of it variables by value, just like
-// bind. `std::ref` can be used to capture references instead.
-// 
-// Synopsis
-// --------
-// 
-//     template<class F>
-//     partial_adaptor<F> partial(F f);
-// 
-// Example
-// -------
-// 
-//     struct sum
-//     {
-//         template<class T>
-//         T sum(T x, T y)
-//         {
-//             return x+y;
-//         }
-//     };
-// 
-//     assert(3 == partial(sum())(1)(2));
-// 
+/// partial
+/// ========
+/// 
+/// Description
+/// -----------
+/// 
+/// The `partial` function adaptor allows partial application of the function.
+/// If the function can not be called with all the parameters, it will return a
+/// function. It will continually do this until the function can be called. By
+/// default, the `partial` captures all of it variables by value, just like
+/// bind. `std::ref` can be used to capture references instead.
+/// 
+/// Synopsis
+/// --------
+/// 
+///     template<class F>
+///     partial_adaptor<F> partial(F f);
+/// 
+/// Example
+/// -------
+/// 
+///     struct sum
+///     {
+///         template<class T>
+///         T sum(T x, T y)
+///         {
+///             return x+y;
+///         }
+///     };
+/// 
+///     assert(3 == partial(sum())(1)(2));
+/// 
 
 #include <fit/conditional.h>
 #include <fit/static.h>

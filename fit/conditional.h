bf0de0f0c637aefba2f617255702ce72d4b051c0
Paul <pfultz2@yahoo.com>
2014-07-28 23:43:24 -0400

    Update documentation for conditional

--- fit/conditional.h
+++ fit/conditional.h
@@ -17,10 +17,10 @@
 /// The `conditional` function adaptor combines several functions together. If the
 /// first function can not be called, then it will try to call the next function.
 /// 
-/// Note: This is different than the `overload` function adaptor, which can lead
-/// to ambiguities. Instead, `conditional` will call the first function that is
-/// callable, regardless if there is another function that could be called as
-/// well. So, for example:
+/// Note: This is different than the `match` function adaptor, which can lead
+/// to ambiguities. Instead, `conditional` will call the first function that
+/// is callable, regardless if there is another function that could be called
+/// as well. So, for example:
 /// 
 ///     struct for_ints
 ///     {

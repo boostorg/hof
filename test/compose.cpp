ddb926d470d31531345232f07517cbea6fb7fa43
Paul <pfultz2@yahoo.com>
2014-08-22 01:49:32 -0400

    Add tests for move-only functions

--- test/compose.cpp
+++ test/compose.cpp
@@ -1,4 +1,5 @@
 #include <fit/compose.h>
+#include <memory>
 #include "test.h"
 
 namespace compose_test {
@@ -20,11 +21,45 @@ struct decrement
     }
 };
 
+struct increment_movable
+{
+    std::unique_ptr<int> n;
+    increment_movable()
+    : n(new int(1))
+    {}
+    template<class T>
+    T operator()(T x) const
+    {
+        return x + *n;
+    }
+};
+
+struct decrement_movable
+{
+    std::unique_ptr<int> n;
+    decrement_movable()
+    : n(new int(1))
+    {}
+    template<class T>
+    T operator()(T x) const
+    {
+        return x - *n;
+    }
+};
+
 FIT_TEST_CASE()
 {
     int r = fit::compose(increment(), decrement(), increment())(3);
     FIT_TEST_CHECK(r == 4);
     static_assert(fit::compose(increment(), decrement(), increment())(3) == 4, "static compose failed");
+}
+
+FIT_TEST_CASE()
+{
+    static_assert(!std::is_copy_constructible<increment_movable>::value && std::is_move_constructible<increment_movable>::value, "Not movable");
+    static_assert(!std::is_copy_constructible<decrement_movable>::value && std::is_move_constructible<decrement_movable>::value, "Not movable");
+    int r = fit::compose(increment_movable(), decrement_movable(), increment_movable())(3);
+    FIT_TEST_CHECK(r == 4);
 }
 
 FIT_TEST_CASE()

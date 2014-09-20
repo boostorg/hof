97d13f5b58084a3ea2737e9b06c7683c610da367
Paul <pfultz2@yahoo.com>
2014-09-20 13:33:03 -0400

    Add a check for this mismatch in returns macro

--- fit/returns.h
+++ fit/returns.h
@@ -17,6 +17,7 @@
 #if FIT_HAS_COMPLETE_DECLTYPE
 #define FIT_RETURNS(...) -> decltype(__VA_ARGS__) { return __VA_ARGS__; } static_assert(true, "")
 #define FIT_THIS this
+#define FIT_CONST_THIS this
 #define FIT_RETURNS_CLASS(...)
 #else
 #include <fit/detail/pp.h>
@@ -29,13 +30,26 @@
 #define FIT_RETURNS_DECLTYPE_CONTEXT(...) FIT_RETURNS_DECLTYPE_CONTEXT_X(FIT_PP_WALL(__VA_ARGS__))
 #define FIT_RETURNS_DECLTYPE_CONTEXT_X(...) __VA_ARGS__
 
-#define FIT_RETURNS_THIS() FIT_PP_IIF(FIT_PP_IS_PAREN(FIT_RETURNS_DECLTYPE_CONTEXT(())))(this, std::declval<fit_this_type>())
+#define FIT_RETURNS_THAT(...) FIT_PP_IIF(FIT_PP_IS_PAREN(FIT_RETURNS_DECLTYPE_CONTEXT(())))(\
+    (fit::detail::check_this<__VA_ARGS__, decltype(this)>(), this), \
+    std::declval<__VA_ARGS__>() \
+)
 
-#define FIT_THIS FIT_PP_RAIL(FIT_RETURNS_THIS)()
+#define FIT_THIS FIT_PP_RAIL(FIT_RETURNS_THAT)(fit_this_type)
+#define FIT_CONST_THIS FIT_PP_RAIL(FIT_RETURNS_THAT)(fit_const_this_type)
 
-#define FIT_RETURNS_CLASS(...) typedef __VA_ARGS__* fit_this_type
+#define FIT_RETURNS_CLASS(...) typedef __VA_ARGS__* fit_this_type; typedef const __VA_ARGS__* fit_const_this_type
 
 #define FIT_RETURNS(...) -> FIT_RETURNS_DECLTYPE(__VA_ARGS__) { FIT_RETURNS_RETURN(__VA_ARGS__); } static_assert(true, "")
+
+namespace fit { namespace detail {
+template<class Assumed, class T>
+struct check_this
+{
+    static_assert(std::is_same<T, Assumed>::value, "Incorret FIT_THIS or FIT_CONST_THIS used.");
+};
+
+}}
 
 #endif
 

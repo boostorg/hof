7286c277a6c2c85c63e5a882e4bd5785b66adf65
Paul <pfultz2@yahoo.com>
2014-10-15 21:05:56 -0400

    Update the delegate constructor for gcc 4.6 to workaround no argument pack expansion in templates

--- fit/detail/delegate.h
+++ fit/detail/delegate.h
@@ -11,6 +11,14 @@
 #include <type_traits>
 #include <utility>
 
+#ifndef FIT_NO_TYPE_PACK_EXPANSION_IN_TEMPLATE
+#if defined(__GNUC__) && !defined (__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 7
+#define FIT_NO_TYPE_PACK_EXPANSION_IN_TEMPLATE 1
+#else
+#define FIT_NO_TYPE_PACK_EXPANSION_IN_TEMPLATE 0
+#endif
+#endif
+
 #define FIT_ENABLE_IF_CONVERTIBLE(...) \
     typename std::enable_if<std::is_convertible<__VA_ARGS__>::value, int>::type = 0
 
@@ -20,9 +28,18 @@
 #define FIT_ENABLE_IF_CONSTRUCTIBLE(...) \
     typename std::enable_if<std::is_constructible<__VA_ARGS__>::value, int>::type = 0
 
+#if FIT_NO_TYPE_PACK_EXPANSION_IN_TEMPLATE
+
+#define FIT_DELGATE_CONSTRUCTOR(C, T, var) \
+    template<class... FitXs, typename fit::detail::enable_if_constructible<T, FitXs...>::type = 0> \
+    constexpr C(FitXs&&... fit_xs) : var(std::forward<FitXs>(fit_xs)...) {}
+    
+#else
 #define FIT_DELGATE_CONSTRUCTOR(C, T, var) \
     template<class... FitXs, FIT_ENABLE_IF_CONSTRUCTIBLE(T, FitXs&&...)> \
     constexpr C(FitXs&&... fit_xs) : var(std::forward<FitXs>(fit_xs)...) {}
+
+#endif
 
 // TODO: For compilers that support inheriting constructors replace with
 // `using Base::Base;`
@@ -30,6 +47,11 @@
 
 namespace fit {
 namespace detail {
+
+template<class X, class... Xs>
+struct enable_if_constructible
+: std::enable_if<std::is_constructible<X, Xs&&...>::value, int>
+{};
 
 template<bool...> struct bool_seq {};
 template<class... Ts>

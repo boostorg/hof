b7e855168c18dab1877a7dd6d123188733ed4d73
Paul <pfultz2@yahoo.com>
2015-02-05 01:08:25 -0500

    Fix tests for capture_forward

--- test/capture.cpp
+++ test/capture.cpp
@@ -1,6 +1,8 @@
 #include "test.h"
 #include <fit/capture.h>
 
+// TODO: Test empty capture
+
 FIT_TEST_CASE()
 {    
     FIT_STATIC_TEST_CHECK(fit::capture(1, 2)(binary_class())() == 3);
@@ -9,10 +11,12 @@ FIT_TEST_CASE()
     FIT_STATIC_TEST_CHECK(fit::capture(1)(binary_class())(2) == 3);
     FIT_TEST_CHECK(fit::capture(1)(binary_class())(2) == 3);
 
-    // FIT_STATIC_TEST_CHECK(fit::capture_forward(1, 2)(binary_class())() == 3);
+    static const int one = 1;
+    static const int two = 2;
+    FIT_STATIC_TEST_CHECK(fit::capture_forward(one, two)(binary_class())() == 3);
     FIT_TEST_CHECK(fit::capture_forward(1, 2)(binary_class())() == 3);
 
-    FIT_STATIC_TEST_CHECK(fit::capture_forward(1)(binary_class())(2) == 3);
+    FIT_STATIC_TEST_CHECK(fit::capture_forward(one)(binary_class())(two) == 3);
     FIT_TEST_CHECK(fit::capture_forward(1)(binary_class())(2) == 3);
 
     FIT_STATIC_TEST_CHECK(fit::capture_decay(1, 2)(binary_class())() == 3);

e16729d766ae500d931fbab180eb5e377de64ecb
Paul <pfultz2@yahoo.com>
2015-04-24 21:21:10 -0500

    Renable the test for now

--- test/args.cpp
+++ test/args.cpp
@@ -2,11 +2,11 @@
 #include <type_traits>
 #include "test.h"
 
-// FIT_TEST_CASE()
-// {
-//     FIT_STATIC_TEST_CHECK(fit::args<3>(1,2,3,4,5) == 3);
-//     FIT_TEST_CHECK( fit::args<3>(1,2,3,4,5) == 3 );
-// }
+FIT_TEST_CASE()
+{
+    FIT_STATIC_TEST_CHECK(fit::args<3>(1,2,3,4,5) == 3);
+    FIT_TEST_CHECK( fit::args<3>(1,2,3,4,5) == 3 );
+}
 
 FIT_TEST_CASE()
 {

5296bb513b28447d7749ee53aa0cc6599b4f0743
Paul <pfultz2@yahoo.com>
2015-05-23 09:57:01 -0500

    Add more tests for unique address

--- test/static_def.h
+++ test/static_def.h
@@ -13,13 +13,16 @@ FIT_STATIC_FUNCTION(fit_sum_lambda) = [](int x, int y)
 
 struct fit_sum_f
 {
-    int operator()(int x, int y) const
+    constexpr int operator()(int x, int y) const
     {
         return x + y;
     }
 };
 
 FIT_STATIC_FUNCTION(fit_sum_fo) = fit_sum_f();
+FIT_STATIC_CONSTEXPR_FUNCTION(fit_sum_constexpr_fo) = fit_sum_f();
+
+FIT_DECLARE_STATIC_VAR(fit_sum_var, fit_sum_f);
 
 // FIT_STATIC_FUNCTION(fit_sum) = [](auto x, auto y) 
 // {
--- test/static_def2.cpp
+++ test/static_def2.cpp
@@ -8,6 +8,12 @@ extern void* f_sum_fo_addr();
 extern void* sum_lambda_addr();
 extern void* sum_fo_addr();
 
+extern void* f_sum_var_addr();
+extern void* f_sum_constexpr_fo_addr();
+
+extern void* sum_var_addr();
+extern void* sum_constexpr_fo_addr();
+
 void* f_sum_lambda_addr()
 {
     return (void*)&fit_test::fit_sum_lambda;
@@ -15,6 +21,15 @@ void* f_sum_lambda_addr()
 void* f_sum_fo_addr()
 {
     return (void*)&fit_test::fit_sum_fo;
+}
+
+void* f_sum_var_addr()
+{
+    return (void*)&fit_test::fit_sum_var;
+}
+void* f_sum_constexpr_fo_addr()
+{
+    return (void*)&fit_test::fit_sum_constexpr_fo;
 }
 
 int f()
@@ -26,6 +41,11 @@ int f()
 #if !FIT_NO_UNIQUE_STATIC_FUNCTION_ADDR
     if (sum_lambda_addr() != f_sum_lambda_addr()) printf("FAILED: Lambda\n");
     if (sum_fo_addr() != f_sum_fo_addr()) printf("FAILED: Function object\n");
+#endif
+
+#if !FIT_NO_UNIQUE_STATIC_VAR
+    if (sum_var_addr() != f_sum_var_addr()) printf("FAILED: Lambda\n");
+    if (sum_constexpr_fo_addr() != f_sum_constexpr_fo_addr()) printf("FAILED: Function object\n");
 #endif
     return 0;
 }

aadbe0317c77fce60c8a8949852c443c74f183f4
Paul <pfultz2@yahoo.com>
2015-05-23 13:53:42 -0500

    Add a non-sfinae version of is_callable

--- fit/is_callable.h
+++ fit/is_callable.h
@@ -39,9 +39,82 @@
 #include <utility>
 #include <type_traits>
 #include <fit/detail/holder.h>
+#include <fit/detail/and.h>
+
+#ifndef FIT_NO_EXPRESSION_SFINAE
+#define FIT_NO_EXPRESSION_SFINAE 0
+#endif
 
 namespace fit {
 
+#if FIT_NO_EXPRESSION_SFINAE
+namespace detail {
+
+struct dont_care
+{
+    dont_care(...);
+};
+
+template<class T>
+struct never_care
+{
+    typedef dont_care type;
+};
+
+struct cant_be_called_type
+{};
+
+template<class F, class... Ts>
+struct is_callable_wrapper : F
+{
+    is_callable_wrapper();
+    typedef cant_be_called_type const &(*pointer_to_function)(typename never_care<Ts>::type...);
+    operator pointer_to_function() const;
+};
+
+template<class T>
+struct not_
+: std::integral_constant<bool, !T::value>
+{};
+
+template<class F, class... Ts>
+struct can_be_called
+: not_<std::is_same<cant_be_called_type, typename std::decay<decltype(
+    is_callable_wrapper<F, Ts...>()(std::declval<Ts>()...)
+)>::type>>
+{};
+
+template<class F, class... Ts>
+struct check_args;
+
+template<class Res, class... Ts, class... Us>
+struct check_args<Res(Us...), Ts...>
+: and_<std::is_convertible<Ts, Us>...>
+{};
+
+template<class Res, class... Ts, class... Us>
+struct can_be_called<Res(*)(Us...), Ts...>
+: std::conditional<sizeof...(Ts) == sizeof...(Us), 
+    check_args<Res(Us...), Ts...>, 
+    std::false_type
+>::type
+{};
+
+template<class Res, class... Ts, class... Us>
+struct can_be_called<Res(Us...), Ts...>
+: std::conditional<sizeof...(Ts) == sizeof...(Us), 
+    check_args<Res(Us...), Ts...>, 
+    std::false_type
+>::type
+{};
+}
+
+template<class F, class... Ts>
+struct is_callable
+: detail::can_be_called<F, Ts...>
+{};
+
+#else
 namespace detail {
 template<class... Ts>
 struct callable_args
@@ -66,11 +139,7 @@ struct is_callable
 : detail::is_callable_impl<F, detail::callable_args<Ts...>>
 {};
 
-// Deprecated but backwards compatible version
-template<class F, class... Ts>
-struct is_callable<F(Ts...)>
-: detail::is_callable_impl<F, detail::callable_args<Ts...>>
-{};
+#endif
 
 }
 

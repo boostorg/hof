002e5e742a325777a183a88c34a248cafdffddb1
Paul <pfultz2@yahoo.com>
2015-05-29 16:09:12 -0500

    Update documention about FIT_STATIC_FUNCTION and FIT_STATIC_LAMBDA_FUNCTION

--- fit/function.h
+++ fit/function.h
@@ -14,8 +14,9 @@
 /// Description
 /// -----------
 /// 
-/// The `FIT_STATIC_FUNCTION` macro allows initializing a function object from
-/// lambdas and adaptors at compile-time in a `constexpr` expression.
+/// The `FIT_STATIC_FUNCTION` macro allows initializing a function object from a
+/// `constexpr` expression. It also ensures that the function object will have a
+/// unique address across translation units. This helps to avoid ODR violations.
 /// 
 /// Example
 /// -------

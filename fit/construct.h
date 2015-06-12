6a8dfeeec909081833b1e7e6e7e7e22119ed5bd6
Paul <pfultz2@yahoo.com>
2015-06-12 18:04:18 -0500

    Add doc for construct

--- fit/construct.h
+++ fit/construct.h
@@ -8,6 +8,39 @@
 #ifndef FIT_GUARD_CONSTRUCT_H
 #define FIT_GUARD_CONSTRUCT_H
 
+/// construct
+/// =========
+/// 
+/// Description
+/// -----------
+/// 
+/// The `construct` function returns a function object that will construct the
+/// object when the called. A template can also be givien, which it will
+/// deduce the parameters to the template.
+/// 
+/// Synopsis
+/// --------
+/// 
+///     template<class T>
+///     constexpr auto construct();
+/// 
+///     template<template<class...> class Template>
+///     constexpr auto construct();
+/// 
+/// Requirements
+/// ------------
+/// 
+/// T or Template must be:
+/// 
+///     MoveConstructible
+/// 
+/// Example
+/// -------
+/// 
+///     auto v = fit::construct<std::vector<int>>()(5, 5);
+///     assert(v.size() == 5);
+/// 
+
 #include <fit/detail/forward.h>
 #include <fit/detail/move.h>
 #include <fit/detail/delegate.h>

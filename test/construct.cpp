bd0243782f5f845e79a1500b653fa7a064d7ffd1
Paul <pfultz2@yahoo.com>
2015-06-12 17:03:44 -0500

    Fix tests for gcc 4.7

--- test/construct.cpp
+++ test/construct.cpp
@@ -5,6 +5,14 @@
 #include <type_traits>
 #include <vector>
 
+template<class T>
+struct ac
+{
+    T value;
+    constexpr ac(T i) : value(i)
+    {}
+};
+
 FIT_TEST_CASE()
 {
     auto v = fit::construct<std::vector<int>>()(5, 5);
@@ -17,5 +25,16 @@ FIT_TEST_CASE()
     auto t = fit::construct<std::tuple>()(1, 2, 3);
     static_assert(std::is_same<std::tuple<int, int, int>, decltype(t)>::value, "");
     FIT_TEST_CHECK(t == std::make_tuple(1, 2, 3));
+// GCC 4.7 doesn't have fully constexpr tuple
+#if defined(__GNUC__) && !defined (__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 8
     FIT_STATIC_TEST_CHECK(std::make_tuple(1, 2, 3) == fit::construct<std::tuple>()(1, 2, 3));
+#endif
+}
+
+FIT_TEST_CASE()
+{
+    auto x = fit::construct<ac>()(1);
+    static_assert(std::is_same<ac<int>, decltype(x)>::value, "");
+    FIT_TEST_CHECK(x.value == ac<int>(1).value);
+    FIT_STATIC_TEST_CHECK(ac<int>(1).value == fit::construct<ac>()(1).value);
 }

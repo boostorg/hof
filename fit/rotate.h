cf0ef580486042a8bb10eba88b1a7549d4736151
Paul <pfultz2@yahoo.com>
2015-08-24 19:04:30 -0500

    Add rotate adaptor

---
+++ fit/rotate.h
@@ -0,0 +1,91 @@
+/*=============================================================================
+    Copyright (c) 2015 Paul Fultz II
+    rotate.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef FIT_GUARD_ROTATE_H
+#define FIT_GUARD_ROTATE_H
+
+/// rotate
+/// ====
+/// 
+/// Description
+/// -----------
+/// 
+/// The `rotate` function adaptor moves the first parameter to the last
+/// parameter.
+/// 
+/// Synopsis
+/// --------
+/// 
+///     template<class F>
+///     rotate_adaptor<F> rotate(F f);
+/// 
+/// Requirements
+/// ------------
+/// 
+/// F must be:
+/// 
+///     FunctionObject
+///     MoveConstructible
+/// 
+/// Example
+/// -------
+/// 
+///     int r = fit::rotate(fit::_ - fit::_)(2, 5);
+///     assert(r == 3);
+/// 
+
+#include <fit/detail/result_of.h>
+#include <fit/reveal.h>
+#include <fit/detail/make.h>
+#include <fit/detail/static_const_var.h>
+
+namespace fit {
+
+template<class F>
+struct rotate_adaptor : F
+{
+    typedef rotate_adaptor fit_rewritable1_tag;
+    FIT_INHERIT_CONSTRUCTOR(rotate_adaptor, F);
+
+    template<class... Ts>
+    constexpr const F& base_function(Ts&&... xs) const
+    {
+        return always_ref(*this)(xs...);
+    }
+
+    struct rotate_failure
+    {
+        template<class Failure>
+        struct apply
+        {
+            template<class T, class... Ts>
+            struct of
+            : Failure::template of<Ts..., T>
+            {};
+        };
+    };
+
+    struct failure
+    : failure_map<rotate_failure, F>
+    {};
+
+    FIT_RETURNS_CLASS(rotate_adaptor);
+
+    template<class T, class... Ts>
+    constexpr FIT_SFINAE_RESULT(const F&, id_<Ts>..., id_<T>) 
+    operator()(T&& x, Ts&&... xs) const FIT_SFINAE_RETURNS
+    (
+        (FIT_MANGLE_CAST(const F&)(FIT_CONST_THIS->base_function(xs...)))
+            (fit::forward<Ts>(xs)..., fit::forward<T>(x))
+    );
+};
+
+FIT_DECLARE_STATIC_VAR(rotate, detail::make<rotate_adaptor>);
+
+}
+
+#endif

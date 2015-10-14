d9bf440612886deb2f91ed7a070065d7025343d9
Paul <pfultz2@yahoo.com>
2015-10-13 23:27:07 -0500

    Try using compressed_pair in capture

--- fit/capture.h
+++ fit/capture.h
@@ -67,22 +67,24 @@ namespace fit {
 namespace detail {
 
 template<class F, class Pack>
-struct capture_invoke : F, Pack
+struct capture_invoke 
+: compressed_pair<F, Pack>
 {
     typedef capture_invoke fit_rewritable1_tag;
-    template<class X, class Y>
-    constexpr capture_invoke(X&& x, Y&& y) : F(fit::forward<X>(x)), Pack(fit::forward<Y>(y))
-    {}
+    typedef compressed_pair<F, Pack> base_type;
+
+    FIT_INHERIT_CONSTRUCTOR(capture_invoke, base_type);
+
     template<class... Ts>
     constexpr const F& base_function(Ts&&... xs) const
     {
-        return always_ref(*this)(xs...);
+        return this->first(xs...);
     }
 
     template<class... Ts>
-    constexpr const Pack& get_pack(Ts&&...xs) const
+    constexpr const Pack& get_pack(Ts&&... xs) const
     {
-        return always_ref(*this)(xs...);
+        return this->second(xs...);
     }
 
     template<class Failure, class... Ts>

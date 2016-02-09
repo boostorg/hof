c370b4b1496d42373d40d41cacbd831b6d05049f
Zach Laine <whatwasthataddress@gmail.com>
2016-02-09 09:06:16 -0600

    In combine() docs, use different int constants for clarity.

--- include/fit/combine.hpp
+++ include/fit/combine.hpp
@@ -44,7 +44,7 @@
 ///         fit::construct<std::tuple>(),
 ///         fit::capture(1)(fit::construct<std::pair>()),
 ///         fit::capture(2)(fit::construct<std::pair>()));
-///     assert(f(2, 4) == std::make_tuple(std::make_pair(1, 2), std::make_pair(2, 4)));
+///     assert(f(3, 7) == std::make_tuple(std::make_pair(1, 3), std::make_pair(2, 7)));
 /// 
 
 #include <fit/pack.hpp>

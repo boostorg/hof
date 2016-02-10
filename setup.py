45658b8f53669ec03bd74c98fb0bda5097ec997e
Paul <pfultz2@yahoo.com>
2016-02-10 13:04:10 -0600

    Use generator expressions instead of list

--- setup.py
+++ setup.py
@@ -5,27 +5,43 @@ prefix = '/// '
 include_dir = 'include/'
 doc_dir = 'doc/src'
 
+def write_to(dir_name, file, lines):
+    open(os.path.join(dir_name, file), 'w').writelines((line + "\n" for line in lines))
+
+
+def extract_section(lines, section):
+    pass
+
 def insert_header_each(line, file):
+    yield line
     if line.startswith('=='):
-        return [line, "\n", "Header\n", "------\n", "\n", "    #include <{0}>\n".format(file), "\n"]
-    else:
-        return [line]
+        yield ""
+        yield "Header"
+        yield "------"
+        yield ""
+        yield "    #include <{0}>".format(file)
+        yield ""
 
 def insert_header(lines, file):
     return [line for x in lines for line in insert_header_each(x, file)]
 
+    
+def write_header(md, name, file):
+    include_file = file[len(include_dir):]
+    write_to(doc_dir, name + '.md', insert_header(md, include_file))
+
 def extract_md(file):
     f = open(file)
-    return [line[len(prefix):] for line in f.readlines() if line.startswith(prefix)]
+    return [line[len(prefix):-1] for line in f.readlines() if line.startswith(prefix)]
 
 for root, subdirs, files in os.walk(include_dir):
     print("root", root)
     if 'detail' not in root:
         for f in files: 
             file = os.path.join(root, f)
-            include_file = file[len(include_dir):]
             md = extract_md(file)
             if len(md) > 0:
                 print('Extracting:', file)
                 name, ext = os.path.splitext(os.path.basename(file))
-                open(os.path.join(doc_dir, name + '.md'), 'w').writelines(insert_header(md, include_file))
+                write_header(md, name, file)
+

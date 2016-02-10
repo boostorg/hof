0e62edaf8c67210632a31fffe6eb47c2c47d760d
Paul <pfultz2@yahoo.com>
2016-02-10 15:50:14 -0600

    Extract and build examples from documentation

---
+++ examples/example.h
@@ -0,0 +1,61 @@
+/*=============================================================================
+    Copyright (c) 2016 Paul Fultz II
+    example.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef FIT_GUARD_EXAMPLE_H
+#define FIT_GUARD_EXAMPLE_H
+
+#include <fit/alias.hpp>
+#include <fit/always.hpp>
+#include <fit/apply_eval.hpp>
+#include <fit/apply.hpp>
+#include <fit/arg.hpp>
+#include <fit/by.hpp>
+#include <fit/capture.hpp>
+#include <fit/combine.hpp>
+#include <fit/compose.hpp>
+#include <fit/compress.hpp>
+#include <fit/conditional.hpp>
+#include <fit/construct.hpp>
+#include <fit/decay.hpp>
+#include <fit/decorate.hpp>
+#include <fit/eval.hpp>
+#include <fit/fix.hpp>
+#include <fit/flip.hpp>
+#include <fit/flow.hpp>
+#include <fit/function.hpp>
+#include <fit/identity.hpp>
+#include <fit/if.hpp>
+#include <fit/implicit.hpp>
+#include <fit/indirect.hpp>
+#include <fit/infix.hpp>
+#include <fit/is_callable.hpp>
+#include <fit/lambda.hpp>
+#include <fit/lazy.hpp>
+#include <fit/lift.hpp>
+#include <fit/limit.hpp>
+#include <fit/match.hpp>
+#include <fit/mutable.hpp>
+#include <fit/pack.hpp>
+#include <fit/partial.hpp>
+#include <fit/pipable.hpp>
+#include <fit/placeholders.hpp>
+#include <fit/protect.hpp>
+#include <fit/repeat.hpp>
+#include <fit/repeat_while.hpp>
+#include <fit/result.hpp>
+#include <fit/returns.hpp>
+#include <fit/reveal.hpp>
+#include <fit/reverse_compress.hpp>
+#include <fit/rotate.hpp>
+#include <fit/static.hpp>
+#include <fit/tap.hpp>
+#include <fit/unpack.hpp>
+
+#include <algorithm>
+#include <cassert>
+
+#endif
--- setup.py
+++ setup.py
@@ -1,31 +1,50 @@
 
-import os
+import os, itertools
 
 prefix = '/// '
 include_dir = 'include/'
 doc_dir = 'doc/src'
+example_dir = 'examples'
 
 def write_to(dir_name, file, lines):
-    open(os.path.join(dir_name, file), 'w').writelines((line + "\n" for line in lines))
+    content = list((line + "\n" for line in lines))
+    if (len(content) > 0):
+        print('Writing file:', file)
+        open(os.path.join(dir_name, file), 'w').writelines(content)
 
+def drop(iterable, n):
+    return itertools.islice(iterable, n, None)
+
+def adjacent(iterable):
+    return itertools.izip_longest(iterable, drop(iterable, 1), fillvalue="")
 
 def extract_section(lines, section):
-    pass
+    dropped = drop(itertools.dropwhile(lambda x:not (x[0] == section and x[1].startswith('--')), adjacent(lines)), 2)
+    taken = itertools.takewhile(lambda x:not (x[1].startswith('--') or x[1].startswith('==')), dropped)
+    return (x for x, y in taken)
 
-def insert_header_each(line, file):
-    yield line
-    if line.startswith('=='):
-        yield ""
-        yield "Header"
-        yield "------"
-        yield ""
-        yield "    #include <{0}>".format(file)
-        yield ""
+def extract_example(lines):
+    example = [x[4:] for x in extract_section(lines, "Example") if x.startswith('    ')]
+    if len(example) > 0:
+        yield '#include "example.h"'
+        yield 'int main() {'
+        for x in example: yield x
+        yield "}"
+
+def write_example(md, name):
+    write_to(example_dir, name + '.cpp', extract_example(md))
 
 def insert_header(lines, file):
-    return [line for x in lines for line in insert_header_each(x, file)]
+    for line in lines:
+        yield line
+        if line.startswith('=='):
+            yield ""
+            yield "Header"
+            yield "------"
+            yield ""
+            yield "    #include <{0}>".format(file)
+            yield ""
 
-    
 def write_header(md, name, file):
     include_file = file[len(include_dir):]
     write_to(doc_dir, name + '.md', insert_header(md, include_file))
@@ -40,8 +59,7 @@ for root, subdirs, files in os.walk(include_dir):
         for f in files: 
             file = os.path.join(root, f)
             md = extract_md(file)
-            if len(md) > 0:
-                print('Extracting:', file)
-                name, ext = os.path.splitext(os.path.basename(file))
-                write_header(md, name, file)
+            name, ext = os.path.splitext(os.path.basename(file))
+            write_header(md, name, file)
+            write_example(md, name)
 

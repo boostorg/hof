cc7ebb047c335078430720349bf2f72e2df3e339
Paul <pfultz2@yahoo.com>
2016-02-18 10:33:44 -0600

    Fix name of is_literal type trait

--- include/fit/detail/intrinsics.hpp
+++ include/fit/detail/intrinsics.hpp
@@ -28,13 +28,13 @@
 #define FIT_IS_CONVERTIBLE(...) __is_convertible_to(__VA_ARGS__)
 #define FIT_IS_CLASS(...) __is_class(__VA_ARGS__)
 #define FIT_IS_EMPTY(...) __is_empty(__VA_ARGS__)
-#define FIT_IS_LITERAL(...) std::is_literal<__VA_ARGS__>::value
+#define FIT_IS_LITERAL(...) std::is_literal_type<__VA_ARGS__>::value
 #else
 #define FIT_IS_CONSTRUCTIBLE(...) std::is_constructible<__VA_ARGS__>::value
 #define FIT_IS_CONVERTIBLE(...) std::is_convertible<__VA_ARGS__>::value
 #define FIT_IS_CLASS(...) std::is_class<__VA_ARGS__>::value
 #define FIT_IS_EMPTY(...) std::is_empty<__VA_ARGS__>::value
-#define FIT_IS_LITERAL(...) std::is_literal<__VA_ARGS__>::value
+#define FIT_IS_LITERAL(...) std::is_literal_type<__VA_ARGS__>::value
 #endif
 
 namespace fit {

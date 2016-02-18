2148824693aacc70ef8d56191ec7e23600594c7e
Paul <pfultz2@yahoo.com>
2016-02-18 17:30:16 -0600

    Take of unused variables in alias test

--- test/alias.cpp
+++ test/alias.cpp
@@ -2,11 +2,17 @@
 #include "test.hpp"
 
 struct foo
-{};
+{
+    int i;
+    foo(int i_) : i(i_)
+    {}
+};
 
 FIT_TEST_CASE()
 {
     fit::alias<int> ai = 5;
-    fit::alias_inherit<foo> af = foo{};
+    FIT_TEST_CHECK(fit::alias_value(ai) == 5);
+    fit::alias_inherit<foo> af = foo{5};
+    FIT_TEST_CHECK(fit::alias_value(af).i == 5);
 }
 

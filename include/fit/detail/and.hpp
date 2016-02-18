9d4bbf6f92707a55e2adc7e06eacddf4457e054e
Paul <pfultz2@yahoo.com>
2016-02-18 17:58:46 -0600

    Fix warning on gcc

--- include/fit/detail/and.hpp
+++ include/fit/detail/and.hpp
@@ -36,7 +36,7 @@ FIT_USING(and_, std::is_same<bool_seq<Ts::value...>, bool_seq<(Ts::value, true).
 template<bool... Bs>
 FIT_USING(and_c, std::is_same<bool_seq<Bs...>, bool_seq<(Bs, true)...>>);
 
-#define FIT_AND_UNPACK(Bs) FIT_IS_BASE_OF(fit::detail::bool_seq<Bs...>, fit::detail::bool_seq<(Bs, true)...>)
+#define FIT_AND_UNPACK(Bs) FIT_IS_BASE_OF(fit::detail::bool_seq<Bs...>, fit::detail::bool_seq<(Bs || true)...>)
 #endif
 
 }} // namespace fit

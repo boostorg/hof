9c91e5cabc2a358c65bcdac21af26985bf2b2b6c
Paul <pfultz2@yahoo.com>
2016-02-24 10:08:12 -0600

    Add file

---
+++ test/returns.cpp
@@ -0,0 +1,19 @@
+#include <fit/returns.hpp>
+#include "test.hpp"
+
+struct add_1
+{
+    int a;
+    add_1() : a(1) {}
+    
+    FIT_RETURNS_CLASS(add_1);
+    
+    template<class T>
+    auto operator()(T x) const 
+    FIT_RETURNS(x+FIT_CONST_THIS->a);
+};
+
+FIT_TEST_CASE()
+{
+    FIT_TEST_CHECK(3 == add_1()(2));   
+}

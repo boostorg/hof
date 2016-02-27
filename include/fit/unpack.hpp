ba59709ea19aa1974e2295e99ac56b142b368b81
Paul <pfultz2@yahoo.com>
2016-02-27 14:01:57 -0600

    Fixes for tuple_get on gcc 4.6

--- include/fit/unpack.hpp
+++ include/fit/unpack.hpp
@@ -294,11 +294,44 @@ make_tuple_gens(const Sequence&)
     return {};
 }
 
+#if (defined(__GNUC__) && !defined (__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 7)
+
+template<std::size_t I, class Tuple>
+struct tuple_element_return
+: std::tuple_element<I, Tuple>
+{};
+
+template<std::size_t I, class Tuple>
+struct tuple_element_return<I, Tuple&>
+: std::add_lvalue_reference<typename tuple_element_return<I, Tuple>::type>
+{};
+
+template<std::size_t I, class Tuple>
+struct tuple_element_return<I, Tuple&&>
+: std::add_rvalue_reference<typename tuple_element_return<I, Tuple>::type>
+{};
+
+template<std::size_t I, class Tuple>
+struct tuple_element_return<I, const Tuple>
+: std::add_const<typename tuple_element_return<I, Tuple>::type>
+{};
+
+template< std::size_t I, class Tuple, class R = typename tuple_element_return<I, Tuple&&>::type >
+R tuple_get( Tuple&& t ) 
+{ 
+    return (R&&)(std::get<I>(fit::forward<Tuple>(t))); 
+}
+#define FIT_UNPACK_TUPLE_GET fit::detail::tuple_get
+#else
+#define FIT_UNPACK_TUPLE_GET std::get
+
+#endif
+
 template<class F, class T, std::size_t ...N>
 constexpr auto unpack_tuple(F&& f, T&& t, seq<N...>) FIT_RETURNS
 (
     f(
-        FIT_AUTO_FORWARD(std::get<N>(FIT_AUTO_FORWARD(t)))...
+        FIT_AUTO_FORWARD(FIT_UNPACK_TUPLE_GET<N>(FIT_AUTO_FORWARD(t)))...
     )
 );
 

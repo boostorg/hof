1d61047f3df9deb000ebb67496ffe5ab047feb43
Paul <pfultz2@yahoo.com>
2016-03-19 00:55:43 -0500

    Fix warnings with clang

--- test/virtual_base.cpp
+++ test/virtual_base.cpp
@@ -3,15 +3,21 @@
 
 struct base { 
     base(int) {} 
-    virtual ~base() {}
+    base(const base&) {}
+    virtual ~base();
 };
+
+base::~base() {}
 
 struct derived : virtual base {
     derived() : base(1) {}
+    derived(const derived&) : base(1) {}
     int operator()(int i, void *) const {
         return i;
     }
+    ~derived();
 };
+derived::~derived() {}
 
 FIT_TEST_CASE()
 {

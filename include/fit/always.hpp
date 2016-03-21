4674466fdcf4baa6d837b86930fb6c32204a56bc
Paul <pfultz2@yahoo.com>
2016-03-21 15:59:09 -0500

    Undocument always_ref for issue #114

--- include/fit/always.hpp
+++ include/fit/always.hpp
@@ -20,17 +20,14 @@
 /// 
 /// The `always` function returns a function object that will always return
 /// the value given to it, no matter what parameters are passed to the
-/// function object. The `always_ref` version will return a reference, and it
-/// requires the value passed in to be an lvalue.
+/// function object.
 /// 
 /// Synopsis
 /// --------
 /// 
 ///     template<class T>
-///     constexpr /* unspecified */ always(T value);
+///     constexpr auto always(T value);
 /// 
-///     template<class T>
-///     constexpr /* unspecified */ always_ref(T& value);
 /// 
 /// Semantics
 /// ---------

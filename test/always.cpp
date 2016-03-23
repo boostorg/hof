a2d4aeddbf496bd9a91846cc946b97a5268b2516
Paul <pfultz2@yahoo.com>
2016-03-23 14:51:57 -0500

    Add tests for always_ref

--- test/always.cpp
+++ test/always.cpp
@@ -16,3 +16,9 @@ FIT_TEST_CASE()
     static_assert(std::is_same<decltype(fit::always()(1, 2)), FIT_ALWAYS_VOID_RETURN>::value, "Failed");
 }
 
+FIT_TEST_CASE()
+{
+    int i = 10; 
+    FIT_TEST_CHECK( fit::always_ref(i)(1,2,3,4,5) == 10 );
+    FIT_TEST_CHECK( &fit::always_ref(i)(1,2,3,4,5) == &i );
+}

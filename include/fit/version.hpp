9e47a0bf3982281d88cec6df3a5bdb5c0d5eb0e1
Paul <pfultz2@yahoo.com>
2016-06-14 15:58:52 -0500

    Add missing version file

---
+++ include/fit/version.hpp
@@ -0,0 +1,16 @@
+/*=============================================================================
+    Copyright (c) 2016 Paul Fultz II
+    version.hpp
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef FIT_GUARD_VERSION_HPP
+#define FIT_GUARD_VERSION_HPP
+
+#define FIT_VERSION_MAJOR 0
+#define FIT_VERSION_MINOR 4
+#define FIT_VERSION_PATCH 0
+#define FIT_VERSION (((FIT_VERSION_MAJOR) << 24) + ((FIT_VERSION_MINOR) << 16) + (FIT_VERSION_PATCH))
+
+#endif

c47bfbace3105d5cffe1556c140f776b3e1f7b14
Paul <pfultz2@yahoo.com>
2016-12-17 20:26:00 -0500

    Noexcept for lift

--- test/lift.cpp
+++ test/lift.cpp
@@ -6,13 +6,21 @@
 #include <algorithm>
 
 template<class T, class U>
-constexpr T sum(T x, U y)
+constexpr T sum(T x, U y) FIT_RETURNS_DEDUCE_NOEXCEPT(x+y)
 {
     return x + y;
 }
 
 FIT_LIFT_CLASS(max_f, std::max);
 FIT_LIFT_CLASS(sum_f, sum);
+
+#if FIT_HAS_NOEXCEPT_DEDUCTION
+FIT_TEST_CASE()
+{
+    static_assert(noexcept(sum_f()(1, 2)), "noexcept lift");
+    static_assert(!noexcept(sum_f()(std::string(), std::string())), "noexcept lift");
+}
+#endif
 
 FIT_TEST_CASE()
 {

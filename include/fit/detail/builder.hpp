c34b967287bd3a0627ba93b5373f6a47bc64c9a0
Paul <pfultz2@yahoo.com>
2017-09-01 01:26:10 -0500

    Convert fix to use adaptor builder

--- include/fit/detail/builder.hpp
+++ include/fit/detail/builder.hpp
@@ -16,6 +16,14 @@
 #include <fit/always.hpp>
 #include <fit/detail/join.hpp>
 
+#if defined(__GNUC__) && !defined (__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 7
+#define FIT_BUILDER_JOIN_BASE_1(...) __VA_ARGS__>::type
+#define FIT_BUILDER_JOIN_BASE(...) fit::detail::compute_builder_base<__VA_ARGS__, FIT_BUILDER_JOIN_BASE_1
+#else
+#define FIT_BUILDER_JOIN_BASE_1(...) <__VA_ARGS__>
+#define FIT_BUILDER_JOIN_BASE(...) __VA_ARGS__::template apply FIT_BUILDER_JOIN_BASE_1
+#endif
+
 namespace fit { namespace detail {
 
 template<class T, class... Ts>
@@ -24,14 +32,14 @@ struct compute_builder_base
     typedef typename T::template apply<Ts...> type;
 };
 
+template<class Adaptor, class... Fs>
+constexpr typename FIT_BUILDER_JOIN_BASE(Adaptor)(Fs...) make_adaptor(Fs... fs) 
+FIT_NOEXCEPT_CONSTRUCTIBLE(typename FIT_BUILDER_JOIN_BASE(Adaptor)(Fs...), Fs&&...)
+{
+    return typename FIT_BUILDER_JOIN_BASE(Adaptor)(Fs...)(fit::move(fs)...);
+}
+
 }} // namespace fit
-#if defined(__GNUC__) && !defined (__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 7
-#define FIT_BUILDER_JOIN_BASE_1(...) __VA_ARGS__>::type
-#define FIT_BUILDER_JOIN_BASE(...) fit::detail::compute_builder_base<__VA_ARGS__, FIT_BUILDER_JOIN_BASE_1
-#else
-#define FIT_BUILDER_JOIN_BASE_1(...) <__VA_ARGS__>
-#define FIT_BUILDER_JOIN_BASE(...) __VA_ARGS__::template apply FIT_BUILDER_JOIN_BASE_1
-#endif
 
 #define FIT_DECLARE_ADAPTOR_USING(adaptor, ...) \
 struct adaptor ## _adaptor : __VA_ARGS__ \
